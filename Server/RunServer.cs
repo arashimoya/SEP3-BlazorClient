0d23e32754a70314dc9d4ead0fe9e8eb3842d7d6
Tymon2115 <tymon.podlowski@gmail.com>
2021-10-29 12:34:15 +0200

    fix

---
+++ Server/RunServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Server{
+    class RunServer{
+        static void Main(string[] args){
+            Console.WriteLine("Start server...");
+            
+            
+        }
+    }
+}
