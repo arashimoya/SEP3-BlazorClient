87657ca4c8cfbf76626c2210df8749322984c730
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-10-29 13:05:30 +0200

    zajebisty commit

--- Server/ClientConnection/Server.cs
+++ Server/ClientConnection/Server.cs
@@ -12,16 +12,15 @@ namespace Server.ClientConnection{
     public class Server : IServer{
         private static IPAddress ip = IPAddress.Parse("127.0.0.1");
         private TcpListener listener = new TcpListener(ip, 5000);
-        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly HttpClient client = new HttpClient();
         
-        public void RunServer(){
+        public async void RunServer(){
             listener.Start();
             while (true){
                 TcpClient tcpClient = listener.AcceptTcpClient();
                 new Thread(() => {
                     NetworkStream stream = tcpClient.GetStream();
                     //byte[] size = new byte[1024];
-
                     byte[] fromClient = new byte[2000];
                     int bytesRead = stream.Read(fromClient, 0, fromClient.Length);
                     string objectAsJson = Encoding.ASCII.GetString(fromClient, 0, bytesRead);
@@ -30,12 +29,18 @@ namespace Server.ClientConnection{
                 }).Start();
             }
         }
-
-        private static async Task ProcessRegistration(string message){
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue(""));
-            httpClient.DefaultRequestHeaders.Add();
+        
+        static async Task PostUser ()
+        {
+            StringContent content = new StringContent(
+            user,
+            Encoding.UTF8,
+            "application/json"
+            );
+            HttpResponseMessage response = await client.PostAsync("http://localhost:8080/api/users/register", content);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(@"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+            else
+                Console.Write("Success");
         }
-    }
 }
