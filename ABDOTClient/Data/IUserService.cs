caa3ae8d759a373b8d2736da751446068dedb9d4
arashimoya <arasimovicadam@gmail.com>
2021-10-29 12:55:14 +0200

    Creating a register component (lacks validation and functionality of sending data)

---
+++ ABDOTClient/Data/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ABDOTClient.Model;
+
+namespace Assignment1.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string email, string password);
+        void RegisterUser(string email, string password, string firstName, string lastName, 
+            string streetAndHouseNumber, string city, string postcode, string country);
+        public bool IsAlreadyInUse(string email);
+    }
+}
