6a91dc2794442ca9e7eb2bc42c6f24acc76a0b39
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-11-28 18:11:46 +0100

    added address to employee

--- ABDOTClient/Model/Employee.cs
+++ ABDOTClient/Model/Employee.cs
@@ -9,11 +9,17 @@ namespace ABDOTClient.Model
         
         public int Role { get; private set; }
         
-        [Required]
-        public string CPR { get; set; }
+        [Required] public string CPR { get; set; }
+
+        [Required] public string Street { get; set; }
         
-        [Required]
-        public DateTime Birthday { get; set; }
+        [Required] public string City { get; set; }
+        
+        [Required] public string Postcode { get; set; }
+        
+        [Required] public string Country { get; set; }
+
+        [Required] public DateTime Birthday { get; set; }
         public IList<Ticket> TicketsSold { get; set; }
 
         public Branch Branch;
